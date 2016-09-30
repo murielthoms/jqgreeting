61afbd7872d16689183c032b710be606f2d5bf11
muriel <murielthoms@gmail.com>
2016-09-30 14:40:18 +0200

    exercice greeting

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log('have fun !');
+// var obj = {};
+// $("button").click(function(){
+
+// obj.prenom = $("#first_name").val();
+// obj.nom = $("#last_name").val();
+// obj.city = $("#city").val();
+
+// $("#username").text(obj.prenom + " " + obj.nom);
+// console.log(obj);
+// });
+
+// console.log(obj);
+
+var obj = {}
+
+$("button").click(function(){
+
+	obj.prenom = $("#first_name").val();
+	obj.nom = $("#last_name").val();
+	obj.city = $("#city").val();
+
+	$("#username").text(obj.prenom + " " + obj.nom);
+
+});
+
+
+
